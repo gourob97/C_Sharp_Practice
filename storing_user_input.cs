e2a1a72b45212cc9b180adfee247a6002057d11a
gourob97 <gourobmazumder507@gmail.com>
2022-06-27 21:15:26 +0600

    storing user input

---
+++ storing_user_input.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+    public static void Main(String[] args){
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you, {name}");
+
+    }
+}
