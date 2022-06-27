78826161e394af6ed8d561b1fbf4e7eb16dcb3e9
gourob97 <gourobmazumder507@gmail.com>
2022-06-27 21:11:32 +0600

    reading user input

---
+++ read_user_input.cs
@@ -0,0 +1,11 @@
+using Internal;
+using System;
+
+class Program{
+    public static void Main(String[] args){
+        Console.WriteLine("What is your favourite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"WOW!! {color} is my favourite color too.");
+
+    }
+}
